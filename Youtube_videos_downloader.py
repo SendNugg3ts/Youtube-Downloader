fdcd3652b0269e9b216a52a4382fe83b847f7c2d
Luís Silva <108976002+SendNugg3ts@users.noreply.github.com>
2022-08-03 01:03:59 +0100

    Add files via upload

---
+++ Youtube_videos_downloader.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+yt.age_restricted
+
+
+print("Autor ", yt.author)
+print("Title ", yt.title)
+print("Views ", yt.views)
+print("Descrição ", yt.description)
+
+yDownload = yt.streams.get_highest_resolution() 
+pc_path=input("Para onde queres que o video seja mandado? ")
+yDownload.download(f"{pc_path}")
+
